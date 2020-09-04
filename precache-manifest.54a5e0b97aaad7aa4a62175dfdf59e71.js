47e91fb8b9f94ddd9d2fe08a984be78c4b9239f0
Joseph Idbensalah <joseph.idbensalah@gmail.com>
2020-09-04 16:04:59 +0100

    Updates

---
+++ precache-manifest.54a5e0b97aaad7aa4a62175dfdf59e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b610722e91681e3ed8108f57f6f7737",
+    "url": "/git-engine/index.html"
+  },
+  {
+    "revision": "8f17d55e0df0f8eebfe6",
+    "url": "/git-engine/static/css/main.890eec8a.chunk.css"
+  },
+  {
+    "revision": "8676ad3d603a2bbf8236",
+    "url": "/git-engine/static/js/2.92ae7eb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-engine/static/js/2.92ae7eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f17d55e0df0f8eebfe6",
+    "url": "/git-engine/static/js/main.23620393.chunk.js"
+  },
+  {
+    "revision": "05d62cc012d6872ce8b7",
+    "url": "/git-engine/static/js/runtime-main.ce7fe3d9.js"
+  }
+]);
